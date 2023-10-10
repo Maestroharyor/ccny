dacdb2d8ec4e2c62a71b2b290043bd284a16170a
maestroharyor <ayomide.odewale1@gmail.com>
2023-10-10 23:42:37 +0100

    Changes

--- src/components/footers/Footer.tsx
+++ src/components/footers/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import { FaFacebook } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <footer className="bg-gray-950">
       <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="flex items-center justify-between gap-1">
           <div className="flex justify-center text-teal-600 sm:justify-start">
             <Link href="/" className={`"navbar-logo block w-full  `}>
               <img
@@ -16,7 +17,17 @@ const Footer = () => {
             </Link>
           </div>
 
-          <p className="mt-4 text-center text-sm text-gray-200 lg:mt-0 lg:text-right">
+          <div>
+            <a
+              href="https://www.facebook.com/CCCOGIYOUTH?mibextid=ZbWKwL"
+              target="_blank"
+              className="text-white text-2xl"
+            >
+              <FaFacebook />
+            </a>
+          </div>
+
+          <p className="sm:mt-4 text-center text-xs sm:text-sm text-gray-200 lg:mt-0 lg:text-right">
             Copyright &copy; {new Date().getFullYear()}. All rights reserved.
           </p>
         </div>
